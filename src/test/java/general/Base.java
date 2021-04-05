6ca08ffb79059393e970d92bf654fc8a4a1e755e
Fabioquermes <fabiomobi@gmail.com>
2021-04-05 00:58:49 -0300

    Realizamos a montagem do arquivo de Passos que  (SearchProduct.java) e o Runner

--- src/test/java/general/Base.java
+++ src/test/java/general/Base.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class Base {
-    protected WebDriver driver; //objeto do Selenium WEbDriver, vulgo bola
+    public WebDriver driver; //objeto do Selenium WEbDriver, vulgo bola
     // ao usar o PageFactory mudar de public para protected
 
     //Criar um construtor e inicializar os elementos do PageFactory
